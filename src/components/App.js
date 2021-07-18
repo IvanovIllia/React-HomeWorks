ea516aa999fd211296e75b78d66d5ba4011114ef
IvanovIllia <ravenolog@rambler.ru>
2021-07-18 19:42:56 +0300

    copy to not lose

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Appartments from './Appartments.js';
+
+function App() {
+  return (
+    <Appartments /> 
+  );
+}
+
+export default App;
---
+++ src/components/myRoom.js
@@ -0,0 +1,4 @@
+import './Chair';
+import './sofa';
+import './table';
+import './wardrobe';

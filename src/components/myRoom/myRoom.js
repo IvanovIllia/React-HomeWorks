058bd0c20536e34ffd16bf249e79f0c7a73445e6
IvanovIllia <ravenolog@rambler.ru>
2021-08-01 22:48:26 +0300

    Add homework

---
+++ src/components/myRoom/myRoom.js
@@ -0,0 +1,22 @@
+import '../Chair/Chair';
+import '../Sofa/sofa';
+import '../Table/table';
+import '../Wardrobe/wardrobe';
+import styles from '../App.css'
+// import {ChairIt} from '../Chair/Chair' 
+
+function MyRoom (){
+    return (
+        <div>
+            <span className="myRoom">
+                My room have:
+            </span> 
+        </div>
+        );
+}
+< Sofa HaveIt = '1'/>;
+< Chair myFunc = {ChairIt}>
+    qqq
+</Chair>
+
+export default myRoom;
